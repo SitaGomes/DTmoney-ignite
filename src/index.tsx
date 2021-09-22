201bca898705f3409cbabb84a2ebb13c324b4cef
SitaGomes <ansitagomes@gmail.com>
2021-09-22 16:29:51 +0100

    Add: inicialize MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createServer} from 'miragejs'
+
 import {Home} from './Pages/Home';
+
+createServer({
+  routes() {
+    this.namespace = '/api'
+
+    this.get('/transactions', () => [
+      {
+        id: Math.random,
+        title: 'Nova SSD',
+        category: 'Withdraw',
+        price: 1000,
+        date: new Date(),
+      }
+    ])
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
